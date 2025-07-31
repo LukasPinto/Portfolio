3633b5dd4cd79bcbd96c6e1b4c9d9807973ca9a4
LukasPinto <lukas.pinto@alumnos.uv.cl>
2025-07-31 06:39:44 -0400

    actualizacion gitignore

--- dist/static/chunks/app_AboutMe_page_tsx_082e80c4._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_@chakra-ui_react_dist_esm_components_683e5ee5._.js"
-],
-    source: "dynamic"
-});
